a380311a26a356ba50ef82ad81175e233ed9dae5
tanakon kabprapun <tanakon.kabprapun@gmail.com>
2022-03-11 23:57:34 +0700

    init

---
+++ utils/text_util.py
@@ -0,0 +1,10 @@
+class text_controls(object):
+    def __init__(self):
+        self.blabla = ''
+
+    def read_text(self, path):
+        txt = open(path, "r")
+        txt.close
+        return txt
+
+    def 
